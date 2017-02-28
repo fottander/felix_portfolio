b6c47d86828f1e47057fb99e2821962dbe60250f
felixottander <felix.ottander@yahoo.com>
2017-02-28 16:28:11 +0100

    Refactored the nav bar to erase active grey buttons. Also added href link to the github links.

--- config.rb
+++ config.rb
@@ -42,3 +42,15 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
+
+require 'extensions/build_cleaner'
+
+configure :build do
+  activate :relative_assets
+  activate :build_cleaner
+end
